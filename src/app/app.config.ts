f4934e06acd16a67c82d44b1aa0514f25ac8d461
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-03-26 17:25:58 +0500

    Added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({
+    'projectId': 'vacationplanningapp-7d4f7',
+    'appId': '1:657987093593:web:b9049a5dbeef63bc8e9d48',
+    'storageBucket': 'vacationplanningapp-7d4f7.appspot.com',
+    'apiKey': 'AIzaSyBnd7P3TamJXSBFDP56sUtRk7DPb5AfS7g',
+    'authDomain': 'vacationplanningapp-7d4f7.firebaseapp.com',
+    'messagingSenderId': '657987093593',
+    'measurementId': 'G-N0LS1KWPCK'
+  }))), importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnd7P3TamJXSBFDP56sUtRk7DPb5AfS7g",
+    authDomain: "vacationplanningapp-7d4f7.firebaseapp.com",
+    projectId: "vacationplanningapp-7d4f7",
+    storageBucket: "vacationplanningapp-7d4f7.appspot.com",
+    messagingSenderId: "657987093593",
+    appId: "1:657987093593:web:b9049a5dbeef63bc8e9d48",
+    measurementId: "G-N0LS1KWPCK"
+  }
+};
