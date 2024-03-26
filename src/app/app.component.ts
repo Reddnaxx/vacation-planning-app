6169a0044f8d73a3315cb1be4189b46138b3847c
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-03-26 18:56:12 +0500

    added firebasemodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FireModule } from './fire.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FireModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
