0e4417dc684f081520b7165c85906fefbf43d529
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-04-03 21:39:36 +0500

    changes theme colors

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { MaterialModule } from "../../modules/material/material.module";
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [HttpClientModule, MaterialModule],
+  imports: [HttpClientModule, MaterialModule, MatToolbar],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
--- src/app/shared/services/icon.service.ts
+++ src/app/shared/services/icon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
-import CustomIcons from "../enums/CustomIcons";
+import CustomIcons from "../enums/custom-icons";
 
 @Injectable({
   providedIn: "root",
