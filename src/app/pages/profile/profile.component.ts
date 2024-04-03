afaabdcc66476d9dcad88469fa3b6910e10952de
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-04-03 21:46:05 +0500

    added reusable button component

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,11 +3,12 @@ import { MaterialModule } from "../../shared/modules/material/material.module";
 import UserModel from "../../shared/models/user.model";
 import { NgOptimizedImage } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: "app-profile",
   standalone: true,
-  imports: [MaterialModule, NgOptimizedImage, FormsModule],
+  imports: [MaterialModule, NgOptimizedImage, FormsModule, ButtonComponent],
   templateUrl: "./profile.component.html",
   styleUrl: "./profile.component.scss",
 })
